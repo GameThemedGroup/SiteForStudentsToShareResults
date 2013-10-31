684a08be422c543d2ace8f1f48e08de3ec41641e
Rodelle Ladia <rladia@gmail.com>
2013-10-31 03:33:09 -0700

    Removed merge artifacts

--- templates/manage-assignments.php
+++ templates/manage-assignments.php
@@ -9,27 +9,6 @@
 get_header(); ?>
 
 <?php
-<<<<<<< HEAD
-require('DBHandler.php');
-global $gtcs12_db;
-
-//echo $_POST['title'] . ' ' . $_POST['description'] . ' ' . $_POST['sample-file'] . ' ' . $_POST['preview-image'];
-if ($_POST) 
-{
-  $courseID = 2;
-  $authorID = 1;
-
-  // create assignment post
-  $assignmentId = $gtcs12_db->CreateAssignment($authorId, $courseId, $_POST['title'], $_POST['description']);
-
-  // prepare paths of images and jar files
-  $imageFileName = $authorId . '_' . $courseId . '_' . $assignmentId . '.png'; // only .png supported now
-  $jarFileName = $authorId . '_' . $courseId . '_' . $assignmentId . '.jar';
-
-  // upload the files and get their destination paths
-  $destFilePaths = $gtcs12_db->UploadFiles("filAssignment", array($imageFileName, $jarFileName));
-}
-=======
 	global $gtcs12_db;
 	
 	$current_user = wp_get_current_user();
@@ -74,58 +53,10 @@ if ($_POST)
 		// upload the files and get their destination paths
 		$destFilePaths = $gtcs12_db->UploadFiles("filAssignment", array($imageFileName, $jarFileName));
 	}
->>>>>>> 803dcb2dffea4690449a18e3035e3c2f9fec6b77
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-<<<<<<< HEAD
-  <form action="" method="post">
-    <div id='create-assignment-box-left'>
-      <div id='pagetitle'>Create Assignment</div>
-      <div id='create-assignment-field'>
-        Title
-        <input class='create-assignment' type="text" name="title" required><br>
-      </div>
-      <div id='create-assignment-field' name='description'>
-        Description
-        <textarea cols="25" rows="10" autocomplete="off" name="description" required></textarea>
-      </div>
-      <div id='create-assignment-field'>
-        Sample File
-        <input class='create-assignment' type="file" name="sample-file[]" required>
-      </div>
-      <input type="submit" value="Submit"/>
-    </div>
-    <div id='create-assignment-box-right'>
-      <div id='create-assignment-field'>
-        Preview Image
-        <input class='create-assignment' type="file" name="preview-image[]" required>
-        <img class='create-assignment' src="<?php bloginfo('template_directory'); ?>/images/blank-project.png">
-      </div>
-    </div>
-  </form>
-
-  <div id='table'>	
-    <table class='manage-courses'>
-    <div id='pagetitle'>Manage Assignments</div>
-      <thead class='manage-courses'>
-        <tr>
-          <th class='manage-courses'>Title</th>
-          <th class='manage-courses'>Date Posted</th>
-          <th class='manage-courses'>Action</th>
-        </tr>
-      </thead>
-      <tbody class='manage-courses'>
-        <tr>
-          <th class='manage-courses'>Lab 1: Draw A Circle</th>
-          <th class='manage-courses'>September 23, 2013</th>
-          <th class='manage-courses'>Delete</th>
-        </tr>
-      </tbody>
-    </table>
-  </div>
-=======
 <?php if($action == "owner error") : ?>
 	<div id="error-box">Error:You don't have ownership of that assignment</div>
 <?php elseif($action == "found error") : ?>
@@ -209,7 +140,6 @@ if ($_POST)
 			</tbody>
 		</table>
 	</div>
->>>>>>> 803dcb2dffea4690449a18e3035e3c2f9fec6b77
 </html>
 
 <?php get_footer() ?>
