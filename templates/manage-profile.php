eef0899229f7e107b9ca98f62297ffe8ecc4a5a7
Andrey Brushchenko <abruschenko@gmail.com>
2013-12-02 14:12:59 -0800

    Added ability to edit password

--- templates/manage-profile.php
+++ templates/manage-profile.php
@@ -12,7 +12,7 @@ get_header(); ?>
 	$currentUser = wp_get_current_user();
 
 	//update user info
-	if($_POST) 
+	if($_POST['op'] == 'info') 
 	{
 		$args = array();
 		
@@ -28,19 +28,38 @@ get_header(); ?>
 		$args['ID'] = $currentUser->ID;
 		wp_update_user($args);
     
-    $action = 'updated';
+    $action = 'profile updated';
 	}
+  elseif($_POST['op'] == 'password') 
+  {
+    $newPass = $_POST['pass'];
+    $newPassConfirm = $_POST['passconfirm'];
+    
+    if(($newPass == $newPassConfirm) && $newPass != '')
+    {
+      //wp_set_password($newPass, $currentUser->ID);
+      $action = 'password changed';
+    }
+    else
+    {
+      $error = 'mismatching passwords';
+    }
+  }
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-<?php if($action == "updated") : ?>
-	<div id="action-box">Profile updated</div>
+<?php if($action != '') : ?>
+	<div id="action-box"><?php echo $action ?></div>
+<?php endif ?>
+
+<?php if($error != '') : ?>
+	<div id="error-box"><?php echo $error ?></div>
 <?php endif ?>
 
   <form action="" method="post">
     <div id='manage-profile-box'>
-      <div id='pagetitle'>Edit Profile</div>	
+      <div id='manage-profile-title'>Edit Profile Information</div>	
       <div id='manage-profile-field'>
         <p class='manage-profile'>Avatar</p>
         <input class="manage-profile" type="file" name="avatar[]">
@@ -57,8 +76,30 @@ get_header(); ?>
         <p class='manage-profile'>Email</p>
         <input class='manage-profile' type="text" name="email" value="<?php echo $currentUser->user_email ?>"><br>
       </div>	
-      <input type="submit" value="Submit"/>
-      <a href="<?php echo site_url('/profile/?user=' . $currentUser->ID) ?>"><button type="button">Cancel</button></a>
+      <div id="manage-profile-buttons">
+        <input type="hidden" name="op" value="info">
+        <input type="submit" value="Submit">
+        <a href="<?php echo site_url('/profile/?user=' . $currentUser->ID) ?>"><button type="button">Cancel</button></a>
+      </div>
+    </div>	
+  </form>
+     
+  <form action="" method="post">
+    <div id='manage-profile-box'>
+      <div id='manage-profile-title'>Edit Password </div>	
+        <div id='manage-profile-field'>
+        <p class='manage-profile'>Password</p>
+        <input class='manage-profile' type="password" name="pass" required><br>
+      </div>	   
+      <div id='manage-profile-field'>
+        <p class='manage-profile'>Confirm Password</p>
+        <input class='manage-profile' type="password" name="passconfirm" required><br>
+      </div>	
+      <div id="manage-profile-buttons">
+        <input type="hidden" name="op" value="password">
+        <input type="submit" value="Submit">
+        <a href="<?php echo site_url('/profile/?user=' . $currentUser->ID) ?>"><button type="button">Cancel</button></a>
+      </div>
     </div>	
   </form>
 </html>
