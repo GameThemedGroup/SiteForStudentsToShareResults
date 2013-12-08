74a7f783456d291a25b2f4603f9f4e52509137dd
Rodelle Ladia <rladia@gmail.com>
2013-12-07 22:22:43 -0800

    whitespace

--- templates/debug.php
+++ templates/debug.php
@@ -10,31 +10,31 @@
 get_header() ?>
 
 <?php
-    global $gtcs12_db;
+global $gtcs12_db;
 
-    $isAdmin = gtcs_user_has_role('administrator');
+$isAdmin = gtcs_user_has_role('administrator');
 
-    if(!$isAdmin) {
-      echo "You do not have permission to view this page. <br />";
-      return;
-    }
+if(!$isAdmin) {
+  echo "You do not have permission to view this page. <br />";
+  return;
+}
 
-    $operation = $_GET['op'];
-    $results = '';
+$operation = $_GET['op'];
+$results = '';
 
-    if ($operation) {
-        if ($operation == 'recreate') {
-            $results = $gtcs12_db->RecreateTables();
-        } else if ($operation == 'del') {
-            $results = $gtcs12_db->DeleteTables();
-        } else if ($operation == 'upload_courses') {
-            $results = upload_courses();
-        } else if ($operation == 'upload_users') {
-            $results = upload_users();
-        } else {
-            $results = 'unknown operation ' . $operation;
-        }
-    }
+if ($operation) {
+  if ($operation == 'recreate') {
+    $results = $gtcs12_db->RecreateTables();
+  } else if ($operation == 'del') {
+    $results = $gtcs12_db->DeleteTables();
+  } else if ($operation == 'upload_courses') {
+    $results = upload_courses();
+  } else if ($operation == 'upload_users') {
+    $results = upload_users();
+  } else {
+    $results = 'unknown operation ' . $operation;
+  }
+}
 
 
 function add_user_from_data($user)
@@ -113,52 +113,50 @@ function upload_courses()
   return "Course Successfully Added";
 }
 ?>
+
 <html>
-    <head></head>
-    <body>
-        <table>
-            <tr>
-                <th>Database operation</th>
-                <th>Operation link</th>
-            </tr>
-            <tr>
-                <td>Upload Users</td>
-                <td>
-                    <form action="?op=upload_users" method="post" enctype="multipart/form-data">
-                    <label for="file">User Data</label>
-                    <input type="file" name="file" id="file">
-                    <input type="submit" name="upload-users" value="Upload">
-                    </form>
-                </td>
-            </tr>
-            <tr>
-                <td>Upload Courses</td>
-                <td>
-                    <form action="?op=upload_courses" method="post" enctype="multipart/form-data">
-                    <label for="file">Course Data</label>
-                    <input type="file" name="file" id="file">
-                    <input type="submit" name="upload-courses" value="Upload">
-                    </form>
-                </td>
-            </tr>
-                        <tr>
-                <td>Recreate Tables</td>
-                <td><a href="<?php echo site_url('/debug/'); ?>?op=recreate">Recreate Tables</a></td>
-            </tr>
-            <tr>
-                <td>Delete Tables</td>
-                <td><a href="<?php echo site_url('/debug/'); ?>?op=del">Delete Tables</a></td>
-            </tr>
-            <tr>
-                <td>Status</td>
-                <td>
-                    <?php
-                        print_r($results);
-                    ?>
-                </td>
-            </tr>
-        </table>
-    </body>
+  <head></head>
+  <body>
+    <table>
+      <tr>
+        <th>Database operation</th>
+        <th>Operation link</th>
+      </tr>
+      <tr>
+        <td>Upload Users</td>
+        <td>
+          <form action="?op=upload_users" method="post" enctype="multipart/form-data">
+          <label for="file">User Data</label>
+          <input type="file" name="file" id="file">
+          <input type="submit" name="upload-users" value="Upload">
+          </form>
+        </td>
+      </tr>
+      <tr>
+        <td>Upload Courses</td>
+        <td>
+          <form action="?op=upload_courses" method="post" enctype="multipart/form-data">
+            <label for="file">Course Data</label>
+            <input type="file" name="file" id="file">
+            <input type="submit" name="upload-courses" value="Upload">
+          </form>
+        </td>
+      </tr>
+      <tr>
+        <td>Recreate Tables</td>
+        <td><a href="<?php echo site_url('/debug/'); ?>?op=recreate">Recreate Tables</a></td>
+      </tr>
+      <tr>
+        <td>Delete Tables</td>
+        <td><a href="<?php echo site_url('/debug/'); ?>?op=del">Delete Tables</a></td>
+      </tr>
+      <tr>
+        <td>Status</td>
+        <td>
+        </td>
+      </tr>
+    </table>
+  </body>
 </html>
 
 <?php get_footer() ?>
