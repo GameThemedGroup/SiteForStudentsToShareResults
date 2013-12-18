b46ef1b30837d169392fe4b3faf4e182b3df1a24
Rodelle Ladia <rladia@gmail.com>
2013-12-17 23:21:37 -0800

    Separated logical operations from the html view
    - Modularized functions
    - Removed undefined index warnings

--- logic/assignments.php
+++ logic/assignments.php
@@ -1,126 +1,138 @@
 <?php
-$current_user = wp_get_current_user();
+$professorId = wp_get_current_user()->ID;
 $isProfessor = gtcs_user_has_role('author');
-$isStudent = gtcs_user_has_role('subscriber');
 
-if(isset($_GET['assignid']))
-{
-  $assignmentId = $_GET['assignid'];
-}
-elseif(isset($_POST['assignid']))
-{
-  $assignmentId = $_POST['assignid'];
+if (!$isProfessor) {
+  echo "You do not have permission to view this page. <br />";
+  return;
 }
 
-if(isset($_GET['courseid']))
-{
-  $courseId = $_GET['courseid'];
-  $courses = $gtcs12_db->GetCourseByFacultyId($current_user->ID);
-}
-elseif($isProfessor)
-{
-  $courses = $gtcs12_db->GetCourseByFacultyId($current_user->ID);
+$courseList = $gtcs12_db->GetCourseByFacultyId($professorId);
 
-  if($courses)
-  {
-    $courseId = $courses[0]->Id;
+$courseId = ifsetor($_GET['id'], null);
+
+if($courseId == null) {
+  $courseId = ifsetor($courseList[0]->Id, null);
+}
+
+$action = ifsetor($_POST['action'], null);
+
+$userFeedback = '';
+$isEditing = false;
+$displayedAssignment = (object) array('post_title' => '', 'post_content' => '');
+
+if ($action == 'edit') {
+  $userFeedback = editAssignmentSetup($displayedAssignment, $isEditing);
+
+} else if ($action != null) {
+  $actionList = array(
+    'delete' => 'deleteAssignment',
+    'create' => 'createAssignment',
+    'update' => 'updateAssignment'
+  );
+
+  if (array_key_exists($actions, $actionList)) {
+    $userFeedback = call_user_func($actionList[$action]);
+  } else {
+    trigger_error("An invalid action was provided.", E_USER_WARNING);
   }
 }
 
-if($courseId == null && $courses != null)
-  $courseId = $courses[0]->Id; // default to first course
+$assignmentList = $gtcs12_db->GetAllAssignments($courseId);
 
-if(!empty($_GET['op']))
-  $getOperation = $_GET['op'];
-else
-  $getOperation = null;
-
-$action = null;
-$assignment = null;
-
-if($getOperation == 'delete') { // delete assignment
-  $action = DeleteAssignment($current_user->ID);
-}
-else if($getOperation == 'edit') // edit assignment(page loads with values from existing assignment)
+function editAssignmentSetup(&$assignment, &$isEditing)
 {
-  if(isset($assignmentId))
-  {
-    $assignment = $gtcs12_db->GetAssignment($assignmentId);
+  $assignmentId = ifsetor($_POST['assignmentId'], null);
+  $isEditing = $assignmentId != null;
+
+  if ($assignmentId == null) {
+    trigger_error(__FUNCTION__ . "
+      - An invalid Assignment ID was provided.",
+      E_USER_WARNING);
+
+    return "There was an error attempting to edit the assignment.";
   }
+
+  global $gtcs12_db;
+  $assignment = $gtcs12_db->GetAssignment($assignmentId);
+  return "Your are now editing the course";
 }
 
-$postOperation = $_POST ? $_POST['op'] : null;
-
-if($postOperation == 'update')
-  $action = UpdateAssignment($current_user->ID, $courseId);
-else if($postOperation == 'create')
+function deleteAssignment()
 {
-  if($isProfessor)
-  {
-    $action = CreateAssignment($current_user->ID, $courseId);
-  }
-  elseif($isStudent)
-  {
-    $title = $_POST['inptTitle'];
-    $description = $_POST['txtDescription'];
+  $professorId = wp_get_current_user()->ID;
+  $assignmentId = ifsetor($_POST['assignmentId'], null);
 
-    $gtcs12_db->CreateSubmission($title, $current_user->ID, $courseId, $assignmentId, $description);
-    $action =  "<b>" . $title . "</b> has been submitted";
-  }
-}
+  $assignment = get_post($assignmentId);
 
-function DeleteAssignment($authorid)
-{
-  if(empty($_GET['assignid'])) // assignment id not give
-    return "No assignment ID provided";
-
-  $assignmentId = $_GET['assignid'];
-  $assignment_info = get_post($assignmentId);
-
-  if(!$assignment_info) // assignment does not exist
+  if(!$assignment) // assignment does not exist
     return "This assignment does not exist";
 
-  if($assignment_info->post_author != $authorid) // user does not have permission to delete assignment
+  // user does not have permission to delete assignment
+  if ($assignment->post_author != $authorid)
     return "You do not have permission to delete this";
 
   wp_delete_post($assignmentId);
   DeleteAttachments($assignmentId, 'jar');
   DeleteAttachments($assignmentId, 'image');
 
-  return "<b>" . $assignment_info->post_title . "</b> has been deleted";
+  return "{$assignment_info->post_title} has been deleted";
 }
 
 // todo check for missing $_POST data
-function UpdateAssignment($authorid, $courseId)
+function updateAssignment($authorid, $courseId)
 {
-  $assignmentId = $_POST['assignid'];
-  $title = $_POST['inptTitle'];
-  $description = $_POST['txtDescription'];
+  $professorId = wp_get_current_user()->ID;
+  $courseId = ifsetor($_POST['courseId'], null);
+  $assignmentId = ifsetor($_POST['assignmentId'], null);
+  $title = ifsetor($_POST['title'], null);
+  $description = ifsetor($_POST['description'], null);
+
   $assignmentLink = '';
   $isEnabled = true;
 
   global $gtcs12_db;
-  $gtcs12_db->UpdateAssignment($assignmentId, $authorid, $courseId, $title, $description, $assignmentLink, $isEnabled);
+  $gtcs12_db->UpdateAssignment(
+    $assignmentId,
+    $professorId,
+    $courseId,
+    $title,
+    $description,
+    $assignmentLink,
+    $isEnabled
+  );
+
   AttachFiles($assignmentId, 'jar', 'jar');
   AttachFiles($assignmentId, 'image', 'image');
 
-  return "<b>" . $title . "</b> has been updated";
+  return "{$title} has been updated";
 }
 
 // todo check for missing $_POST data
-function CreateAssignment($authorid, $courseId)
+function createAssignment()
 {
-  $title = $_POST['inptTitle'];
-  $description = $_POST['txtDescription'];
+  $professorId = wp_get_current_user()->ID;
+  $courseId = ifsetor($_POST['courseId'], null);
+  $title = ifsetor($_POST['title'], null);
+  $description = ifsetor($_POST['description'], null);
+
   $assignmentLink = '';
   $isEnabled = true;
 
   global $gtcs12_db;
-  $assignmentId = $gtcs12_db->CreateAssignment($authorid, $courseId, $title, $description, $assignmentLink, $isEnabled);
+  $assignmentId = $gtcs12_db->CreateAssignment(
+    $professorId,
+    $courseId,
+    $title,
+    $description,
+    $assignmentLink,
+    $isEnabled
+  );
+
   AttachFiles($assignmentId, 'jar', 'jar');
   AttachFiles($assignmentId, 'image', 'image');
 
-  return "<b>" . $title . "</b> has been created";
+  return "{$title} has been created";
 }
 
 // Checks the $_FILES array for images and attaches them to the given assignment
@@ -133,7 +145,8 @@ function AttachFiles($assignmentId, $fileIndex, $attachmentType)
 {
   global $gtcs12_db;
 
-  if(file_exists($_FILES[$fileIndex]['tmp_name']) && is_uploaded_file($_FILES[$fileIndex]['tmp_name'])) {
+  if (   file_exists($_FILES[$fileIndex]['tmp_name'])
+      && is_uploaded_file($_FILES[$fileIndex]['tmp_name'])) {
     DeleteAttachments($assignmentId, $attachmentType);
     $title = pathinfo($_FILES[$fileIndex]['name'], PATHINFO_FILENAME);
     $isImage = ($attachmentType == "image");
--- templates/assignments.php
+++ templates/assignments.php
@@ -8,49 +8,88 @@
  */
 get_header(); ?>
 
+<?php
+/* Expected State Variables:
+ ******************************************************************************
+ * userFeedback - Message to display to user after a successful or failed
+ *   action. Set to '' if there is no feedback to display.
+ *
+ * course - an object containing the following fields
+ *   Name - the course name
+ *   Description - the course description
+ *   Quarter - text represntation of the academic quarter. Ex. Summer, Spring
+ *
+ * courseList - a list of courses beloging to the professor.
+ *   $courseList == null if the professor has no courses
+ *
+ * quarterList - an array of academic quarters in the form
+ *   (quarter => selectStatus).
+ *   Ex. "Summer" => true
+ *       "Fall"   => false
+ *   The user will be able to select between Summer and Fall with Summber being
+ *   the default selection.
+ *
+ * url[] - array containing urls to the following pages
+ *   courses  => this page
+ *   my-class => templates/my-class.php
+ *
+ * isEditing - true if the user is editing a course
+ */
+?>
+
 <?php include_once(get_template_directory() . '/logic/assignments.php'); ?>
 
 <!DOCTYPE html>
 <html lang="en">
-<?php if($action != null) : ?>
-  <div id="action-box"><?php echo $action ?></div>
+<?php if($action) : ?>
+  <div id="action-box"><?php echo $userFeedback; ?></div>
 <?php endif ?>
 
-<form action="<?php echo get_permalink() . "?courseid={$courseId}"; ?>" method="post" enctype="multipart/form-data">
+<form action="<?php echo get_permalink(); ?>" method="post" enctype="multipart/form-data">
   <div id='create-assignment-box'>
     <div id='create-assignment-title'>
-      <?php echo ($getOperation == 'edit') ? "Edit Assignment" : "Create Assignment"; ?>
+      <?php if ($isEditing): ?>Edit Assignment
+      <?php else: ?>Create Assignment
+      <?php endif; ?>
     </div>
+
     <div id='create-assignment-field'>
       <p class="create-assignment">Title</p>
-      <input class='create-assignment' type="text" name="inptTitle"
-        value="<?php echo $assignment ? $assignment->post_title : ''; ?>" required>
+      <input class='create-assignment' type="text" name="title"
+        value="<?php echo $displayedAssignment->post_title; ?>" required>
     </div>
+
     <div id='create-assignment-field'>
       <p class="create-assignment">Description</p>
-      <?php $descriptionValue = $assignment ? $assignment->post_content : ''; ?>
-      <textarea cols="25" rows="10" autocomplete="off" name="txtDescription" required><?php echo $descriptionValue; ?></textarea>
+      <textarea cols="25" rows="5" name="description" required><?php echo $displayedAssignment->post_content; ?></textarea>
     </div>
+
     <div id='create-assignment-field'>
       <p class="create-assignment">Sample File</p>
       <input class='create-assignment' type="file" name="jar">
     </div>
+
     <div id='create-assignment-field'>
       <p class="create-assignment">Preview Image</p>
       <input class='create-assignment' type="file" name="image" accept="image/*">
     </div>
+
     <div id="create-assignment-buttons">
-  <?php if($getOperation == 'create' || $getOperation == 'delete' || $getOperation == '') : ?>
-      <input type="hidden" name="op" value="create">
-      <input type="submit" value="Create"/>
-  <?php elseif($getOperation == 'edit') : ?>
-      <input type="hidden" name="op" value="update">
-      <input type="submit" value="Finish Editing"/>
-  <?php endif ?>
-      <input type="hidden" name="assignid" value="<?php echo $assignmentId ?>">
-      <input type="hidden" name="courseid" value="<?php echo $courseId ?>">
-      <a href="<?php echo site_url('/my-class/?id=' . $courseId) ?>"><button type="button">Cancel</button></a>
-    </div>
+
+      <input type="hidden" name="assignId" value="<?php echo $assignmentId; ?>">
+      <input type="hidden" name="courseId" value="<?php echo $courseId; ?>">
+
+    <?php if ($isEditing): ?>
+        <input type="hidden" name="action" value="update">
+        <input type="submit" value="Finish Editing"/>
+    <?php else: ?>
+        <input type="hidden" name="action" value="create">
+        <input type="submit" value="Create"/>
+    <?php endif; ?>
+      <a href="<?php echo site_url('assignments/?id=' . $courseId) ?>">
+        <button type="button">Cancel</button>
+      </a>
+    </div> <!-- create-assignment-buttons -->
   </div> <!-- Create-Assignment-Box -->
 </form>
 
@@ -58,14 +97,14 @@ get_header(); ?>
   <div id="sidebar-menu">
     <div id="sidebar-menu-title">Courses</div>
     <ul class="sidebar-menu">
-<?php if($courses) : ?>
-<?php   foreach($courses as $course) : ?>
+<?php if($courseList) : ?>
+<?php   foreach($courseList as $course) : ?>
 <?php   if($courseId == $course->Id) : ?>
           <li class="sidebar-menu-selected">
 <?php   else : ?>
           <li class="sidebar-menu">
 <?php   endif ?>
-        <a class="sidebar-menu" href="<?php echo site_url('/assignments/?courseid=' . $course->Id) ?>">
+        <a class="sidebar-menu" href="<?php echo site_url('/assignments/?id=' . $course->Id) ?>">
           <p class="sidebar-menu-top"><?php echo $course->Name ?></p>
           <p class="sidebar-menu-bottom"><?php echo $course->Quarter . ', ' . $course->Year ?></p>
         </a>
@@ -75,7 +114,7 @@ get_header(); ?>
       <li class="sidebar-menu-center">You have no courses</li>
 <?php endif ?>
     </ul>
-  </div>
+  </div> <!-- sidebar-menu -->
 
 <div id='table'>
   <div id='table-title'>Manage Assignments</div>
@@ -88,36 +127,46 @@ get_header(); ?>
       </tr>
     </thead>
     <tbody>
-      <?php $assignments = $gtcs12_db->GetAllAssignments($courseId) ?>
-      <?php if($assignments) : ?>
-        <?php foreach($assignments as $assignment) : ?>
-          <?php $assignLink = site_url('/assignment/?assignid=' . $assignment->AssignmentId); ?>
-            <tr>
-              <th><a href="<?php echo $assignLink ?>"><?php echo $assignment->Title ?></a></th>
-              <th><?php echo date('F d, Y', strtotime($assignment->Date)); ?></th>
-              <th>
-                <form action="<?php echo site_url('/assignments/') ?>" method="get">
-                  <select name="op">
-                    <option disabled="disabled" selected>Choose an action</option>
-                    <option value="edit">Edit</option>
-                    <option value="delete">Delete</option>
-                  </select>
-                  <input type="hidden" name="assignid" value="<?php echo $assignment->AssignmentId ?>">
-                  <input type="hidden" name="courseid" value="<?php echo $courseId ?>">
-                  <input type="submit" value="Confirm"/>
-                </form>
-              </th>
-            </tr>
-        <?php endforeach; ?>
+      <?php if(!$assignmentList) : ?>
+        <tr>
+         <th class="center" colspan="3">You have no assignments for this course</th>
+        </tr>
       <?php else: ?>
-            <tr>
-              <th class="center" colspan="3">You have no assignments for this course</th>
-            </tr>
+      <?php foreach($assignmentList as $assignment) : ?>
+        <?php $assignLink = site_url('/assignment/?assignid=' . $assignment->AssignmentId); ?>
+          <tr>
+            <th><a href="<?php echo $assignLink; ?>">
+               <?php echo $assignment->Title ?></a>
+            </th>
+
+            <th><?php echo date('F d, Y', strtotime($assignment->Date)); ?></th>
+            <th>
+              <form action="
+                <?php echo site_url('/assignments/?id=') . $courseId; ?>" method="post">
+
+                <select name="action">
+                  <option disabled="disabled" selected>Choose an action</option>
+                  <option value="edit">Edit</option>
+                  <option value="delete">Delete</option>
+                </select>
+
+                <input type="hidden" name="assignmentId"
+                  value="<?php echo $assignment->AssignmentId; ?>">
+
+                <input type="hidden" name="courseId"
+                  value="<?php echo $courseId; ?>">
+
+                <input type="submit" value="Confirm"/>
+
+              </form>
+            </th>
+          </tr>
+      <?php endforeach; ?>
       <?php endif; ?>
     </tbody>
   </table>
 </div>
-<?php endif ?>
+<?php endif; ?>
 </html>
 
 <?php get_footer(); ?>
