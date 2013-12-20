09b1a82391f0ac7b3d5692977da79819724baf28
Rodelle Ladia <rladia@gmail.com>
2013-12-19 22:48:33 -0800

    Added function to easily check if variables are set
    Several methods follow the pattern of reading variables from $_POST or $_GET
    and failing if any of them are not correctly set. This function makes it
    simpler to trigger warnings for these unset variables and allow the calling
    function to return an error.

--- common/global_functions.php
+++ common/global_functions.php
@@ -17,6 +17,41 @@ function ifsetor(&$variable, $default = null) {
   return $tmp;
 }
 
+// Returns false and triggers a warning for any value in $args that is null
+//
+// @param function The name of the calling function, ie. __FUNCTION__
+// @param filePath The path to the file of the calling function, ie. __FILE__
+// @param line     The line number of calling function, ie. __LINE__
+// @param args     associative array containing variableName => value of the
+//                 values to validate
+// Example
+//   $id    = ifsetor($_GET['id', null);
+//   $title = ifsetor($_GET['title', null);
+//
+//   $isValid = gtcs_validate_not_null(__FUNCTION__, __FILE__, __LINE__,
+//      compact('id', 'title'));
+function gtcs_validate_not_null($function, $filePath, $line, $args)
+{
+  // limit the display of the filePath
+  // ex. re/al/ly/lo/ng/pa/th/file.php
+  // becomes pa/th/file.php
+  $numParentDirs = 3;
+  $dirs = explode('/', $filePath);
+  $file = implode('/', array_slice($dirs, -$numParentDirs, $numParentDirs, true));
+
+  $isValid = true;
+  foreach ($args as $variable => $value) {
+    if ($value == null) {
+      trigger_error("{$variable} not provided. {$function} in {$file}:{$line}",
+        E_USER_WARNING);
+
+      $isValid = false;
+    }
+  }
+
+  return $isValid;
+}
+
 /**
  * Checks if a particular user has a role.
  * Returns true if a match was found.
