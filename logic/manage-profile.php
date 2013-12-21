0acf3acba4d61c93430ba5e3dffdae17ec7ab9e6
Rodelle Ladia <rladia@gmail.com>
2013-12-21 02:58:57 -0800

    Updated to reflect style guide

--- logic/manage-profile.php
+++ logic/manage-profile.php
@@ -1,38 +1,32 @@
 <?php
-	$currentUser = wp_get_current_user();
+	$user = wp_get_current_user();
 
 	//update user info
-	if($_POST['op'] == 'info')
-	{
+	if ($_POST['action'] == 'info') {
 		$args = array();
 
-		if($_POST['firstname'])
+		if ($_POST['firstname'])
 			$args['first_name'] = $_POST['firstname'];
 
-		if($_POST['lastname'])
+		if ($_POST['lastname'])
 			$args['last_name'] = $_POST['lastname'];
 
-		if($_POST['email'])
+		if ($_POST['email'])
 			$args['user_email'] = $_POST['email'];
 
 		$args['ID'] = $currentUser->ID;
 		wp_update_user($args);
 
-    $action = 'profile updated';
-	}
-  elseif($_POST['op'] == 'password')
-  {
+    $userFeedback = 'profile updated';
+	} elseif ($_POST['action'] == 'password') {
     $newPass = $_POST['pass'];
     $newPassConfirm = $_POST['passconfirm'];
 
-    if(($newPass == $newPassConfirm) && $newPass != '')
-    {
+    if(($newPass == $newPassConfirm) && $newPass != '') {
       //wp_set_password($newPass, $currentUser->ID);
-      $action = 'password changed';
-    }
-    else
-    {
-      $error = 'mismatching passwords';
+      $userFeedback = 'password changed';
+    } else {
+      $userFeedback = 'mismatching passwords';
     }
   }
 ?>
--- templates/manage-profile.php
+++ templates/manage-profile.php
@@ -10,61 +10,63 @@ get_header(); ?>
 
 <?php include_once(get_template_directory() . '/logic/manage-profile.php'); ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<?php if($action != '') : ?>
-	<div id="action-box"><?php echo $action ?></div>
-<?php endif ?>
+<?php if ($userFeedback != ''): ?>
+	<div id="action-box"><?php echo $userFeedback; ?></div>
+<?php endif; ?>
 
-<?php if($error != '') : ?>
-	<div id="error-box"><?php echo $error ?></div>
-<?php endif ?>
-
-  <form action="" method="post">
-    <div id='manage-profile-box'>
-      <div id='manage-profile-title'>Edit Profile Information</div>
-      <div id='manage-profile-field'>
-        <p class='manage-profile'>Avatar</p>
-        <input class="manage-profile" type="file" name="avatar[]">
-      </div>
-      <div id='manage-profile-field'>
-        <p class='manage-profile'>First Name</p>
-        <input class='manage-profile' type="text" name="firstname" value="<?php echo $currentUser->user_firstname ?>"><br>
-      </div>
-      <div id='manage-profile-field'>
-        <p class='manage-profile'>Last Name</p>
-        <input class='manage-profile' type="text" name="lastname" value="<?php echo $currentUser->user_lastname ?>"><br>
-      </div>
-      <div id='manage-profile-field'>
-        <p class='manage-profile'>Email</p>
-        <input class='manage-profile' type="text" name="email" value="<?php echo $currentUser->user_email ?>"><br>
-      </div>
-      <div id="manage-profile-buttons">
-        <input type="hidden" name="op" value="info">
-        <input type="submit" value="Submit">
-        <a href="<?php echo site_url('/profile/?user=' . $currentUser->ID) ?>"><button type="button">Cancel</button></a>
-      </div>
+<!-- Edit Profile Form -->
+<form action="" method="post">
+  <div id='manage-profile-box'>
+    <div id='manage-profile-title'>Edit Profile Information</div>
+    <div id='manage-profile-field'>
+      <p class='manage-profile'>Avatar</p>
+      <input class="manage-profile" type="file" name="avatar[]">
     </div>
-  </form>
+    <div id='manage-profile-field'>
+      <p class='manage-profile'>First Name</p>
+      <input class='manage-profile' type="text" name="firstname"
+        value="<?php echo $user->user_firstname ?>" /><br />
+    </div>
+    <div id='manage-profile-field'>
+      <p class='manage-profile'>Last Name</p>
+      <input class='manage-profile' type="text" name="lastname"
+        value="<?php echo $user->user_lastname ?>" /><br />
+    </div>
+    <div id='manage-profile-field'>
+      <p class='manage-profile'>Email</p>
+      <input class='manage-profile' type="text" name="email"
+        value="<?php echo $user->user_email ?>" /><br />
+    </div>
+    <div id="manage-profile-buttons">
+      <input type="hidden" name="action" value="info">
+      <input type="submit" value="Submit">
+      <a href="<?php echo site_url('/profile/?user=' . $user->ID) ?>">
+        <button type="button">Cancel</button></a>
+    </div>
+  </div>
+</form>
+<!-- Edit Profile Form -->
 
-  <form action="" method="post">
-    <div id='manage-profile-box'>
-      <div id='manage-profile-title'>Edit Password </div>
-        <div id='manage-profile-field'>
+<!-- Edit Password Form -->
+<form action="" method="post">
+  <div id='manage-profile-box'>
+    <div id='manage-profile-title'>Edit Password </div>
+      <div id='manage-profile-field'>
         <p class='manage-profile'>Password</p>
         <input class='manage-profile' type="password" name="pass" required><br>
       </div>
-      <div id='manage-profile-field'>
-        <p class='manage-profile'>Confirm Password</p>
-        <input class='manage-profile' type="password" name="passconfirm" required><br>
-      </div>
-      <div id="manage-profile-buttons">
-        <input type="hidden" name="op" value="password">
-        <input type="submit" value="Submit">
-        <a href="<?php echo site_url('/profile/?user=' . $currentUser->ID) ?>"><button type="button">Cancel</button></a>
-      </div>
+    <div id='manage-profile-field'>
+      <p class='manage-profile'>Confirm Password</p>
+      <input class='manage-profile' type="password" name="passconfirm" required><br>
     </div>
-  </form>
-</html>
+    <div id="manage-profile-buttons">
+      <input type="hidden" name="action" value="password">
+      <input type="submit" value="Submit">
+      <a href="<?php echo site_url('/profile/?user=' . $user->ID) ?>">
+        <button type="button">Cancel</button></a>
+    </div>
+  </div>
+</form>
+<!-- Edit Password Form -->
 
 <?php get_footer(); ?>
